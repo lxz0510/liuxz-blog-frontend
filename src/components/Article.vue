1dd68a0b7f46224583fa44f5596f6e6893323c01
Hongzhuang Liu <robliu@boolvector.com>
2022-11-30 16:45:30 +0800

    add vue router

--- src/components/Article.vue
+++ src/components/Article.vue
@@ -1,11 +1,35 @@
 <script setup lang="ts">
+import { ref, computed, watch, watchEffect } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
 import Author from './Author.vue'
+import { useFetch } from '@vueuse/core'
+const router = useRouter()
+const route = useRoute()
+
+watchEffect(async () => {
+  await useFetch(`${route.params.id}`)
+})
+const currentArticleId = computed(() => route.params.id)
+const nextPost = computed(() => Number(currentArticleId.value) - 1)
+
+const prevPost = computed(() => Number(currentArticleId.value) + 1)
+
+const handlePostClick = (id: any) => {
+  router.push({
+    name: 'article',
+    params: {
+      id: id
+    },
+  })
+}
+
 </script>
 
 <template>
-  <article class="xl:divide-y xl:divide-gray-200 dark:xl:divide-slate-200/5">
-    <header class="pt-6 xl:pb-10 space-y-1 text-center">
-      <h1 class="
+  <Transition>
+    <article class="xl:divide-y xl:divide-gray-200 dark:xl:divide-slate-200/5">
+      <header class="pt-6 xl:pb-10 space-y-1 text-center">
+        <h1 class="
           text-3xl
           leading-9
           font-extrabold
@@ -14,11 +38,11 @@ import Author from './Author.vue'
           sm:text-4xl sm:leading-10
           md:text-5xl md:leading-14
         ">
-        {{ " data.title" }}
-      </h1>
-    </header>
+          {{ " data.title" }}
+        </h1>
+      </header>
 
-    <div class="
+      <div class="
         divide-y
         xl:divide-y-0
         divide-gray-200 dark:divide-slate-200/5
@@ -26,38 +50,35 @@ import Author from './Author.vue'
         pb-16
         xl:pb-20
       " style="grid-template-rows: auto 1fr">
-      <Author />
-      <div class="divide-y divide-gray-200 dark:divide-slate-200/5 xl:pb-0 xl:col-span-3 xl:row-span-2">
-        <Content class="prose dark:prose-invert max-w-none pt-10 pb-8" />
-      </div>
+        <Author />
+        <div class="divide-y divide-gray-200 dark:divide-slate-200/5 xl:pb-0 xl:col-span-3 xl:row-span-2">
+          <!-- <Content class="prose dark:prose-invert max-w-none pt-10 pb-8" /> -->
+        </div>
 
-      <footer class="
+        <footer class="
           text-sm
           font-medium
           leading-5
           divide-y divide-gray-200 dark:divide-slate-200/5
           xl:col-start-1 xl:row-start-2
         ">
-        <div class="py-8">
-          <h2 class="text-xs tracking-wide uppercase text-gray-500 dark:text-white">
-            Next Article
-          </h2>
-          <div class="link">
-
+          <div class="py-8" v-if="nextPost">
+            <h2 class="text-xs tracking-wide uppercase text-gray-500 dark:text-white"
+              @click="handlePostClick(nextPost)">
+              Next Article
+            </h2>
           </div>
-        </div>
-        <div class="py-8">
-          <h2 class="text-xs tracking-wide uppercase text-gray-500 dark:text-white">
-            Previous Article
-          </h2>
-          <div class="link">
-
+          <div class="py-8" v-if="prevPost">
+            <h2 class="text-xs tracking-wide uppercase text-gray-500 dark:text-white"
+              @click="handlePostClick(prevPost)">
+              Previous Article
+            </h2>
           </div>
-        </div>
-        <div class="pt-8">
-          <router-link class="link" to="/">← Back to the blog</router-link>
-        </div>
-      </footer>
-    </div>
-  </article>
+          <div class="pt-8">
+            <router-link class="link" to="/">← Back to the blog</router-link>
+          </div>
+        </footer>
+      </div>
+    </article>
+  </Transition>
 </template>
--- src/layout/Layout.vue
+++ src/layout/Layout.vue
@@ -1,7 +1,3 @@
-<script setup lang="ts">
-import Home from '../components/Home.vue'
-</script>
-
 <template>
   <div class="antialiased dark:bg-slate-900">
     <div class="max-w-3xl mx-auto px-4 sm:px-6 xl:max-w-5xl xl:px-0">
